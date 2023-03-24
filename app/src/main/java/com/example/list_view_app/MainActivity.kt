9cfd783dc1752f4b299f3c4fc9165b4ed7a20f0c
farisadlin-kargo <106053488+farisadlin-kargo@users.noreply.github.com>
2023-03-24 10:37:24 +0700

    Added a new array list from resources

--- app/src/main/java/com/example/list_view_app/MainActivity.kt
+++ app/src/main/java/com/example/list_view_app/MainActivity.kt
@@ -5,16 +5,15 @@ import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-    private val items = listOf(
-        Item("Item 1", "Description 1"),
-        Item("Item 2", "Description 2"),
-        Item("Item 3", "Description 3"),
-        Item("Item 4", "Description 4")
-    )
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val itemsA = resources.getStringArray(R.array.example_names)
+        val itemsB = resources.getStringArray(R.array.example_descriptions)
+        val items = itemsA.zip(itemsB).map { (name, description) ->
+            Item(name, description)
+        }
 
         val itemAdapter = ItemAdapter(items)
         val itemList = findViewById<RecyclerView>(R.id.item_list)
